907c0e6466c525c077a9cb42ee707757d056f643
Zach VanOast <zach.vanoast@tylertech.com>
2021-01-30 14:43:34 -0500

    fixed dumb rebirth stats

--- src/main.py
+++ src/main.py
@@ -32,13 +32,14 @@ async def on_message(message):
             profile = await result[0].profile(Title.ModernWarfare, Mode.Warzone)
 
             level = profile["level"]
-            kills = profile["weekly"]["mode"]["br_rebirth_rbrthquad"]["properties"]["kills"]
-            deaths = profile["weekly"]["mode"]["br_rebirth_rbrthquad"]["properties"]["deaths"]
-            kd = profile["weekly"]["mode"]["br_rebirth_rbrthquad"]["properties"]["kdRatio"]
+            kills = profile["weekly"]["mode"]["br_mini_rebirth_mini_royale_quads"]["properties"]["kills"]
+            deaths = profile["weekly"]["mode"]["br_mini_rebirth_mini_royale_quads"]["properties"]["deaths"]
+            kd = profile["weekly"]["mode"]["br_mini_rebirth_mini_royale_quads"]["properties"]["kdRatio"]
+            killsPerGame = profile["weekly"]["mode"]["br_mini_rebirth_mini_royale_quads"]["properties"]["killsPerGame"]
 
             #wl = profile["weekly"]["mode"]["br_rebirth_rbrthquad"]["properties"]["wins"] / profile["lifetime"]["mode"]["br_all"]["properties"]["gamesPlayed"]
 
-            await message.channel.send(f"Zoney rebirth quads weekly stats: \n{result[0].username} ({result[0].platform.name}) Level: {level}\nKills: {kills}, Deaths: {deaths}, K/D Ratio: {kd}")
+            await message.channel.send(f"Zoney rebirth quads weekly stats: \n{result[0].username} ({result[0].platform.name}) Level: {level}\nKills: {kills}, Deaths: {deaths}, K/D Ratio: {kd}, Kills per game: {killsPerGame}")
             #await message.
         except ValueError:
             await message.channel.send("The proper usage is '!codtest USERNAME#1111'")
