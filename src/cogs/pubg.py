7841b3bd0b77e2b680d8612e0db2da8f57385533
Zach VanOast <zach.vanoast@gmail.com>
2022-01-23 02:38:19 -0500

    PUBG modifier basics

---
+++ src/cogs/pubg.py
@@ -0,0 +1,92 @@
+import random
+from discord.ext import commands
+
+# Guns
+normalGuns = [
+    'AR',
+    'Sniper',
+    'SMG',
+    'DMR',
+    'LMG'
+]
+
+rareGuns = [
+    'Crossbow',
+    'EMT Gear'
+]
+
+# Gun loadout "overrides"
+normalOverrides = [
+    'Keep the first guns you find.',
+    'Shotty snipes!'
+]
+
+rareOverrides = [
+    'Pistols ONLY.'
+]
+
+# Modifiers
+mods = [
+    'You must keep guns you get a kill with.',
+    'Crate hunters!',
+    'MAD MAX!',
+    'GO BANGA!'
+]
+
+class PUBG(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command(
+        brief='Bored of PUBG? Mix it up!',
+        description='Bored of PUBG? Mix it up!')
+    async def pubg(self, ctx):
+        msg = 'Your loadout:\n'
+        chosenGuns = []
+
+        # 20% chance to override guns
+        if random.randrange(1,5) == 1:
+            overrides = normalOverrides
+        
+            if random.randrange(1,5) == 1:
+                overrides += rareOverrides
+
+            msg += random.choice(overrides)
+        else:
+            # 5% chance for each rare gun to be selected
+            availableRareGuns = rareGuns[:]
+            random.shuffle(availableRareGuns)
+            for gun in availableRareGuns:
+                if len(chosenGuns) == 2:
+                    break
+                if random.randrange(1,20) == 1:
+                    chosenGuns.append(gun)
+                    availableRareGuns.remove(gun)
+
+            # Then fill in empty guns with normal guns
+            availableGuns = normalGuns[:]
+            random.shuffle(availableGuns)
+            while len(chosenGuns) < 2:
+                randomGun = random.choice(availableGuns)
+                chosenGuns.append(randomGun)
+                availableGuns.remove(randomGun)
+
+            msg += chosenGuns[0] + ' + ' + chosenGuns[1]
+        
+        # Each mod has less of a chance, starting at 10%
+        modChance = 10
+        activeMods = []
+        random.shuffle(mods)
+        for mod in mods:
+            if random.randrange(1,modChance) == 1:
+                activeMods.append(mod)
+                modChance *= 2
+        
+        if len(activeMods) >= 1:
+            msg += '\n\nActive mods:\n' +  ' '.join(activeMods)
+
+        await ctx.message.channel.send(msg)
+
+def setup(bot):
+    bot.add_cog(PUBG(bot))
