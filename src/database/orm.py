d9a592a7062e0397102d0fb4ea22e60d6a6dec87
Zach VanOast <zach.vanoast@gmail.com>
2022-01-29 23:11:24 -0500

    SQLAlchemy + auto-bant feature w/ link tracking

---
+++ src/database/orm.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, String, Integer, DateTime
+
+from database.database import Base
+
+class Link(Base):
+    __tablename__ = 'links'
+    id = Column(Integer, primary_key=True)
+    url = Column(String)
+    user = Column(String)
+    channel = Column(String)
+    date = Column(DateTime)
+    jump_url = Column(String)
+
+    def __init__(self, url, user, channel, date, jump_url):
+        self.url = url
+        self.user = user
+        self.channel = channel
+        self.date = date
+        self.jump_url = jump_url
+
+class StartupHistory(Base):
+    __tablename__ = 'startup_history'
+    id = Column(Integer, primary_key=True)
+    date = Column(DateTime)
+
+    def __init__(self, date):
+        self.date = date
