e2a4c1679978b91ab3b90086fdf3f656c93bf963
Zach VanOast <zach.vanoast@gmail.com>
2022-01-29 23:33:00 -0500

    fixed DB env variables

--- src/database/database.py
+++ src/database/database.py
@@ -5,7 +5,9 @@ from sqlalchemy.orm import sessionmaker
 
 user = os.getenv('DBPASS')
 pw = os.getenv('DBUSER')
-engine = create_engine("postgresql://{0}:{1}@localhost:5432/banga".format(pw,user))
+host_name = os.getenv('DBHOST')
+db_name = os.getenv('DBNAME')
+engine = create_engine("postgresql://{0}:{1}@{2}:5432/{3}".format(pw,user,host_name,db_name))
 
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
